8de17272c62da09d8e7fa03a540136f68e7fdd76
divocbn <divocbn@gmail.com>
2025-06-09 21:52:41 +0200

    fix: migrate the project to pnpm due that bun isnt supported

--- apps/proxy/src/index.ts
+++ apps/proxy/src/index.ts
@@ -1,4 +1,4 @@
-import Fastify, { FastifyRequest, FastifyReply, FastifyError, errorCodes } from 'fastify';
+import Fastify, { FastifyRequest } from 'fastify';
 import rateLimit from '@fastify/rate-limit';
 import fastifyProxy from '@fastify/http-proxy';
 import { redis } from '@ddos-protection/redis';
@@ -7,28 +7,30 @@ const server = Fastify({
   logger: true
 });
 
-// note(module): apparently generates a redis key with "fastify-rate-limit-(ip)" - so for reset the ratelimit we can delete that? 
-await server.register(rateLimit, {
-  max: 10,
-  timeWindow: '1 minute',
-  redis: redis,
-  keyGenerator: (request: FastifyRequest) => request.ip,
-});
+async function main() {
+  // note(module): apparently generates a redis key with "fastify-rate-limit-(ip)" - so for reset the ratelimit we can delete that? 
+  await server.register(rateLimit, {
+    max: 10,
+    timeWindow: '1 minute',
+    redis: redis,
+    keyGenerator: (request: FastifyRequest) => request.ip,
+  });
 
-await server.register(fastifyProxy, {
-  upstream: 'http://habbo.de',
-  prefix: '/test',
-  http2: false
-});
+  await server.register(fastifyProxy, {
+    upstream: 'http://habbo.de',
+    prefix: '/test',
+  });
 
-server.get('/health', async () => {
-  return { status: "ok" };
-});
+  server.get('/health', async () => {
+    return { status: "ok" };
+  });
+
+  await server.listen({ port: 3002, host: '0.0.0.0' });
+  console.log('Server is running on http://localhost:3002');
+}
 
 try {
-  await server.listen({ port: 3002, host: '0.0.0.0' });
-
-  console.log('Server is running on http://localhost:3002');
+  main();
 } catch (err) {
   server.log.error(err);
   process.exit(1);
