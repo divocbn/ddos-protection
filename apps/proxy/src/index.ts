26adb8c96b8921d4282f269d76c0670526b53500
divocbn <divocbn@gmail.com>
2025-06-09 18:35:23 +0200

    feat: proxy app using fastify

---
+++ apps/proxy/src/index.ts
@@ -0,0 +1,23 @@
+import Fastify, { FastifyRequest, FastifyReply } from 'fastify';
+import rateLimit from '@fastify/rate-limit';
+
+const server = Fastify({
+  logger: true
+});
+
+await server.register(rateLimit, {
+  max: 100,
+  timeWindow: '1 minute'
+});
+
+server.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+  return { "test": 0 };
+});
+
+try {
+  await server.listen({ port: 3002, host: '0.0.0.0' });
+  console.log('Server is running on http://localhost:3002');
+} catch (err) {
+  server.log.error(err);
+  process.exit(1);
+} 
